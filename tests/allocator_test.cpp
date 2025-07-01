1dca0509285d2749db81f0e90c681f29c18db298
Danny15278 <danny26574@gmail.com>
2025-07-01 20:06:49 +1200

    added unit tests

--- tests/allocator_test.cpp
+++ tests/allocator_test.cpp
@@ -1,8 +1,33 @@
-// tests/allocator_test.cpp
 
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch_test_macros.hpp>
+#include "../src/allocator.hpp"
 
-TEST_CASE("Dummy Test") {
-	REQUIRE(1 + 1 == 2);
+
+TEST_CASE("Allocate returns non-null", "[MemoryPool]") {
+	MemoryPool pool(64, 10);
+	void* ptr = pool.allocate();
+	REQUIRE(ptr != nullptr);
 }
+
+TEST_CASE("Reuse deallocated memory", "[MemoryPool]") {
+	MemoryPool pool(64, 10);
+	void* ptr1 = pool.allocate();
+	pool.deallocate(ptr1);
+	void* ptr2 = pool.allocate();
+	REQUIRE(ptr2 == ptr1); 
+}
+
+TEST_CASE("Returns nullptr after all chunks allocated", "[MemoryPool]") {
+	MemoryPool pool(64, 2);
+	void* ptr1 = pool.allocate();
+	void* ptr2 = pool.allocate();
+	void* ptr3 = pool.allocate(); 
+	
+	(void)ptr1;
+	(void)ptr2;
+	REQUIRE(ptr3 == nullptr);
+	REQUIRE(pool.allocate() == nullptr);
+}
+
+
